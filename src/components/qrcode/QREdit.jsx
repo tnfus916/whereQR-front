162113e48aab9a2c9baaaea312d4d80d960be61e
tnfus916 <tnfus1999@gmail.com>
2023-10-09 09:40:31 +0900

    Feat: Qrcode edit page create

--- src/components/qrcode/QREdit.jsx
+++ src/components/qrcode/QREdit.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import {
@@ -8,27 +7,17 @@ import {
   Button,
   QRPageContainer,
   QRFormContainer,
+  QRForm,
 } from "./QRStyle";
+import axios from "axios";
+import axiosInstance from "../../api/api";
 
 function QREdit() {
   const navigate = useNavigate();
-  const { id } = useParams();
-
-  const BaseURL = "http://localhost:8080/qrcode/";
-
-  const axiosInstance = axios.create({
-    baseURL: BaseURL,
-    timeout: 5000,
-    headers: {
-      Authorization: "Token",
-      "Content-Type": "application/json",
-      accept: "application/json",
-    },
-  });
+  const id = useParams();
 
   const [title, setTitle] = useState("No Title");
   const [memo, setMemo] = useState("No result");
-  const [address, setAdd] = useState("No address");
   const [phonenum, setPhonenum] = useState("No result");
 
   const onChangeMemo = (e) => {
@@ -37,52 +26,51 @@ function QREdit() {
   const onChangeTitle = (e) => {
     setTitle(e.target.value);
   };
-  const onChangeAdd = (e) => {
-    setAdd(e.target.value);
-  };
   const onChangePhone = (e) => {
     setPhonenum(e.target.value);
   };
-
-  useEffect(() => {
-    axios
-      .get("http://localhost:8080/qrcode/data/", { params: { id: id } })
-      .then((response) => {
-        console.log(response);
-
-        if (Number(response.data["is_null"]) === 0) {
-          setTitle(response.data["title"]);
-          setAdd(response.data["address"]);
-          setMemo(response.data["memo"]);
-          setPhonenum(response.data["phonenumber"]);
-        } else {
-          console.log("등록된 정보가 없습니다.");
-        }
-      });
-  }, []);
 
   const onSubmit = (e) => {
     e.preventDefault();
 
     const data = {
-      id: id,
+      id: id["ID"],
       title: title,
-      text: memo,
-      address: address,
+      memo: memo,
       phonenum: phonenum,
     };
 
-    if (localStorage.getItem("token")) {
-      //로그인을 안 했다면, 로그인이 필요한 페이지라고 경고.
-      let token = localStorage.getItem("token");
-      axiosInstance.defaults.headers["Authorization"] = "Token " + token;
-      axiosInstance.post("modifyQR/", data).then(() => {
-        navigate(`/UserQr`);
+    axios
+      .post("http://localhost:8080/qrcode/update", { params: data })
+      .then((res) => {
+        console.log(res);
+        navigate(`/mypage`);
       });
-    } else {
-      alert("login필요");
-    }
+
+    // if (localStorage.getItem("token")) {
+    //   //로그인을 안 했다면, 로그인이 필요한 페이지라고 경고.
+    //   let token = localStorage.getItem("token");
+    //   axiosInstance.defaults.headers["Authorization"] = "Token " + token;
+    //   axiosInstance.post("modifyQR/", data).then(() => {
+    //     navigate(`/UserQr`);
+    //   });
+    // } else {
+    //   alert("login필요");
+    // }
   };
+
+  useEffect(() => {
+    axiosInstance
+      .get("/qrcode/scan/", { params: { id: id["ID"] } })
+      .then((res) => {
+        console.log(res);
+
+        setTitle(res.data["title"]);
+        setMemo(res.data["memo"]);
+        setPhonenum(res.data["phonenumber"]);
+      });
+  }, []);
+
   return (
     <>
       <QRPageContainer className="modifyQR">
@@ -116,7 +104,7 @@ function QREdit() {
             />
             <br />
             <Button className="button" type="primary" onClick={onSubmit}>
-              변경하기
+              수정하기
             </Button>
           </QRForm>
         </QRFormContainer>

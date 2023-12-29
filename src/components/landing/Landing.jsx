8b6aee79f098e15e8fa837cab9c5c4941725c7af
tnfus916 <tnfus1999@gmail.com>
2023-12-29 15:17:23 +0900

    Fix: error in import code and set to double quote

--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -2,9 +2,10 @@ import {
   ContentsContainer,
   ImageContainer,
   LandingContainer,
-} from "./LandingStyle";
   GuideContainer,
   Text,
+} from './LandingStyle';
+
 import qrcode from '../../assets/qrcode_image.png';
 import title from '../../assets/landing_title.png';
 import descript from '../../assets/landing_descript.png';
@@ -18,30 +19,30 @@ function Landing() {
       <LandingContainer>
         <ContentsContainer>
           <ImageContainer>
-            <img src={title} width='160' alt='' />
+            <img src={title} width="160" alt="" />
           </ImageContainer>
           <ImageContainer>
-            <img src={qrcode} width='160' alt='' />
+            <img src={qrcode} width="160" alt="" />
           </ImageContainer>
           <ImageContainer>
-            <img src={descript} width='160' alt='' />
+            <img src={descript} width="160" alt="" />
           </ImageContainer>
           <GuideContainer>
             <Text>사용자 가이드 </Text>
             <ImageContainer>
-              <img src={user_guide} width='160' alt='' />
+              <img src={user_guide} width="160" alt="" />
             </ImageContainer>
           </GuideContainer>
           <GuideContainer>
             <Text>습득자 가이드 </Text>
             <ImageContainer>
-              <img src={helper_guide} width='160' alt='' />
+              <img src={helper_guide} width="160" alt="" />
             </ImageContainer>
           </GuideContainer>
           <GuideContainer>
             <Text>커뮤니티</Text>
             <ImageContainer>
-              <img src={community} width='160' alt='' />
+              <img src={community} width="160" alt="" />
             </ImageContainer>
           </GuideContainer>
         </ContentsContainer>

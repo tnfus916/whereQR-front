b36b99301dc54f5536269a321af4213c7508dbdf
baeksoojin <qsoojinq@naver.com>
2023-12-30 20:35:05 +0900

    fix : kakao login parameter setting

--- src/components/account/KakaoLogin.jsx
+++ src/components/account/KakaoLogin.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useState } from 'react';
+import { useState , useEffect} from 'react';
 
 function KakaoLogin() {
   const [kakaoAccessToken, setKakaoAccessToken] = useState(null);
@@ -8,65 +8,36 @@ function KakaoLogin() {
   const [accessToken, setAccessToken] = useState(null);
 
   const axiosInstance = axios.create({
-    baseURL: 'http://27.96.130.127:8080',
+    baseURL: 'http://localhost:8080',
     timeout: 5000,
     headers: {
       'Content-Type': 'application/json',
     },
   });
 
-  const code = new URL(document.location).searchParams.get('code');
-  console.log('code', code);
+  useEffect(() => {
+    const codeFromUrl = new URL(document.location).searchParams.get('code');
 
-  // 카카오 토큰 받아오기
-  axiosInstance
-    .get('/member/kakao/token', code)
+    console.log('axios params', { params: { code: codeFromUrl } });
+    axiosInstance
+    .get('/member/kakao/token', { params: { code: codeFromUrl} })
     .then((res) => {
+      console.log(res);
       console.log(res.data.status);
-      if (res.status === 'SUCCESS') {
+      if (res.data.status === 'SUCCESS') {
         setKakaoAccessToken(res.data.accessToken);
         console.log('kakaoAccessToken', kakaoAccessToken);
       } else {
+        console.log('error');
         window.alert(res.data.message);
       }
     })
     .catch((err) => {
       console.log('get kakao token error', err);
     });
+  }, []);
+  
 
-  // 카카오 아이디와 이름 받아오기
-  axiosInstance
-    .get('/member/kakao/me', kakaoAccessToken)
-    .then((res) => {
-      setKakaoId(res.data.id);
-      setUsername(res.data.kakao_account.profile.nickname);
-    })
-    .catch((err) => {
-      console.log('get kakaoId and username error', err);
-    });
-
-  // 카카오 아이디로 로그인
-  axiosInstance
-    .post('member/kakao/login', kakaoId)
-    .then((res) => {
-      if (res.data.errorType === 'NOT_FOUND') {
-        window.location.href = `/signup?kakaoid=${kakaoId}&username=${username}`; // 회원가입 페이지로 이동
-      } else if (res.data.errorType === 'TokenExpiredException') {
-        // 토큰 만료시 토큰 재발급
-        axios
-          .post('/member/auth/refresh', localStorage.getItem('token'))
-          .then((res) => {
-            setAccessToken(res.data.accessToken);
-            localStorage.setItem('token', res.data.refreshToken);
-          });
-      } else {
-        setAccessToken(res.data.accessToken);
-        localStorage.setItem('token', res.data.refreshToken);
-      }
-    })
-    .catch((err) => {
-      console.log('whereqr login error', err);
-    });
   return <div>KakaoLogin</div>;
 }
 

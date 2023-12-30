bda6697e0cce0e854dbb8643ed7df8cd17afba77
tnfus916 <tnfus1999@gmail.com>
2023-12-30 19:34:45 +0900

    Fix: add kakaologin component

--- Router.jsx
+++ Router.jsx
@@ -1,15 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Landing from "./src/components/landing/Landing";
-import NotFound from "./src/components/notfound/NotFound";
-import Layout from "./src/components/layout/Layout";
-import Join from "./src/components/account/Join";
-import MyPage from "./src/components/account/MyPage";
-import QRScan from "./src/components/qrcode/QRScan";
-import QRDetail from "./src/components/qrcode/QRDetail";
-import QREdit from "./src/components/qrcode/QREdit";
-import QRSave from "./src/components/qrcode/QRSave";
-import QRRegister from "./src/components/qrcode/QRRegister";
-import Login from "./src/components/account/Login";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Landing from './src/components/landing/Landing';
+import NotFound from './src/components/notfound/NotFound';
+import Layout from './src/components/layout/Layout';
+import Join from './src/components/account/Join';
+import MyPage from './src/components/account/MyPage';
+import QRScan from './src/components/qrcode/QRScan';
+import QRDetail from './src/components/qrcode/QRDetail';
+import QREdit from './src/components/qrcode/QREdit';
+import QRSave from './src/components/qrcode/QRSave';
+import QRRegister from './src/components/qrcode/QRRegister';
+import Login from './src/components/account/Login';
+import Chatlist from './src/page/Chatlist';
+import Chatroom from './src/page/Chatroom';
+import KakaoLogin from './src/components/account/KakaoLogin';
+
 function Router() {
   return (
     <BrowserRouter>
@@ -20,6 +24,7 @@ function Router() {
           <Route path="/qrscan/:ID" element={<QRDetail />} />
           <Route path="/signup" element={<Join />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/kakaologin" element={<KakaoLogin />} />
           <Route path="/mypage" element={<MyPage />} />
           <Route path="/qrsave" element={<QRSave />} />
           <Route path="/qrsave/:ID" element={<QRRegister />} />
--- src/components/account/Login.jsx
+++ src/components/account/Login.jsx
@@ -1,93 +1,37 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import { useNavigate } from 'react-router-dom';
 import {
   AccountPageContainer,
   AccountForm,
   AccountFormContainer,
   ButtonContainer,
-} from "./AccountStyle";
+} from './AccountStyle';
 
-import kakaoLogin from "../../assets/kakao_login_medium_narrow.png";
+import kakaoLogin from '../../assets/kakao_login_medium_narrow.png';
 
 // // 쿠키를 주고받기 위해 설정 ( 쿠키가 브라우저에 제대로 저장된 것이 맞는지 확인위함)
 // axios.defaults.withCredentials = true;
 
-function Login({}) {
+function Login() {
   // oauth 요청 URL
-  const Rest_api_key = "271b6b6b673acb0d6daca27769150dbc"; //REST API KEY
-  const redirect_uri = "http://localhost:3000"; //Redirect URI
+  const Rest_api_key = '271b6b6b673acb0d6daca27769150dbc'; //REST API KEY
+  const redirect_uri = 'http://27.96.130.127:3000/kakaologin'; //Redirect URI
   const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
 
-  const [kakaoAccessToken, setKakaoAccessToken] = useState(null);
-  const [kakaoId, setKakaoId] = useState(null);
-  const [username, setUsername] = useState(null);
-  const [accessToken, setAccessToken] = useState(null);
-
-  const axiosInstance = axios.create({
-    baseURL: "http://27.96.130.127:8080",
-    timeout: 5000,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-
-  const handleLogin = () => {
+  const handleKakaoLogin = () => {
     window.location.href = kakaoURL;
-    const code = new URL(window.location.href).searchParams.get("code");
-
-    // 카카오 토큰 받아오기
-    axiosInstance
-      .get("/member/kakao/token", code)
-      .then((res) => {
-        setKakaoAccessToken(res.data.accessToken);
-      })
-      .catch((err) => {
-        console.log("get kakao token error", err);
-      });
-
-    // 카카오 아이디와 이름 받아오기
-    axiosInstance
-      .get("/member/kakao/me", kakaoAccessToken)
-      .then((res) => {
-        setKakaoId(res.data.id);
-        setUsername(res.data.kakao_account.profile.nickname);
-      })
-      .catch((err) => {
-        console.log("get kakaoId and username error", err);
-      });
-
-    // 카카오 아이디로 로그인
-    axiosInstance
-      .post("member/kakao/login", kakaoId)
-      .then((res) => {
-        if (res.data.errorType === "NOT_FOUND") {
-          window.location.href = `/signup?kakaoid=${kakaoId}&username=${username}`; // 회원가입 페이지로 이동
-        } else if (res.data.errorType === "TokenExpiredException") {
-          // 토큰 만료시 토큰 재발급
-          axios
-            .post("/member/auth/refresh", localStorage.getItem("token"))
-            .then((res) => {
-              setAccessToken(res.data.accessToken);
-              localStorage.setItem("token", res.data.refreshToken);
-            });
-        } else {
-          setAccessToken(res.data.accessToken);
-          localStorage.setItem("token", res.data.refreshToken);
-        }
-      })
-      .catch((err) => {
-        console.log("whereqr login error", err);
-      });
   };
 
   return (
     <>
-      <AccountPageContainer className="signin">
+      <AccountPageContainer>
         <AccountFormContainer>
           <AccountForm>
             <ButtonContainer>
-              <img src={kakaoLogin} alt="kakaoLogin" onClick={handleLogin} />
+              <img
+                src={kakaoLogin}
+                alt="kakaoLogin"
+                onClick={handleKakaoLogin}
+              />
             </ButtonContainer>
           </AccountForm>
         </AccountFormContainer>

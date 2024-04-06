8926ae53b9ad30dab15a32369cd5369276c0f6d7
Suryun Park <tnfus1999@gmail.com>
2024-04-06 17:14:54 +0900

    Style: Change in guide container

--- src/page/Landing.jsx
+++ src/page/Landing.jsx
@@ -4,68 +4,58 @@ import styled from 'styled-components';
 import qrcode from '../assets/qrcode_image.svg';
 import title from '../assets/landing_title.svg';
 import descript from '../assets/landing_descript.svg';
-import user_guide from '../assets/user_guide.svg';
-import helper_guide from '../assets/helper_guide.svg';
-import community from '../assets/community.svg';
 
 export const LandingContainer = styled.div`
+  padding-top: 50px;
   padding-bottom: 100px;
   width: 100%;
-  height: 500%;
   display: flex;
   justify-content: center;
   ${({ darkness }) => `
-    background: linear-gradient(to bottom, rgba(255, 255, 255, ${darkness}), rgba(224, 238, 251, ${darkness}));
+    background: linear-gradient(to bottom, rgba(255, 255, 255, ${darkness}), rgba(188, 206, 251, ${darkness}));
   `}
 `;
 
 export const ContentsContainer = styled.div`
   text-align: center;
-  height: 80%;
-  width: 90%;
+  height: 100%;
+  width: 50%;
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
   align-items: center;
+  justify-content: center;
   padding: 5px;
 `;
 
-export const ImageContainer = styled.div`
-  margin-top: 30px;
-  margin-bottom: 20px;
-  display: flex;
-  justify-content: space-evenly;
+export const Image = styled.img`
+  width: 9rem;
+  margin-bottom: 50px;
 `;
 
 export const GuideContainer = styled.div`
+  margin-top: 1rem;
   width: 100%;
-  height: 20%;
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  padding: 20px;
-  margin-top: 10px;
-  margin-bottom: 20px;
-  ${ImageContainer} {
-    width: 90%;
-    margin-top: 10px;
-    justify-content: flex-start;
-  }
+  align-items: flex-start;
+  justify-content: center;
+  background-color: rgba(255, 255, 255, 0.4);
+  padding: 1.2rem;
+  border-radius: 10px;
+  box-shadow: rgba(83, 83, 83, 0.2) 0px 8px 20px;
+`;
+
+export const Title = styled.div`
+  margin-bottom: 0.7rem;
+  font-size: 0.85rem;
+  font-weight: 800;
+  color: rgb(96, 147, 249);
 `;
 
 export const Text = styled.div`
-  width: 100%;
-  height: 20%;
-  display: flex;
-  justify-content: start;
-  align-items: center;
-  font-size: 0.85rem;
-  font-weight: 800;
-  margin-left: 5px;
-  margin-top: 7px;
-  margin-bottom: 5px;
-  color: #6093f9;
+  font-size: 0.9rem;
+  line-height: 1.3rem;
+  text-align: left;
 `;
 
 function Landing() {
@@ -92,32 +82,20 @@ function Landing() {
     <>
       <LandingContainer darkness={ darkness }>
         <ContentsContainer>
-          <ImageContainer>
-            <img src={ title } width="160" alt="" />
-          </ImageContainer>
-          <ImageContainer>
-            <img src={ qrcode } width="160" alt="" />
-          </ImageContainer>
-          <ImageContainer>
-            <img src={ descript } width="160" alt="" />
-          </ImageContainer>
+          <Image src={ title } width="160" alt="" />
+          <Image src={ qrcode } width="160" alt="" />
+          <Image src={ descript } width="160" alt="" />
           <GuideContainer>
-            <Text>사용자 가이드 </Text>
-            <ImageContainer>
-              <img src={ user_guide } width="160" alt="" />
-            </ImageContainer>
+            <Title>사용자 가이드 </Title>
+            <Text>물건 분실에 대비해 소지품에 QR코드를 부착해 물건 분실 시 보다 손쉽게 물건을 찾을 수 있어요.</Text>
           </GuideContainer>
           <GuideContainer>
-            <Text>습득자 가이드 </Text>
-            <ImageContainer>
-              <img src={ helper_guide } width="160" alt="" />
-            </ImageContainer>
+            <Title>습득자 가이드 </Title>
+            <Text>분실물을 습득한 후 QR코드를 스캔하면 물건 주인에게 연락이 가능해요.</Text>
           </GuideContainer>
           <GuideContainer>
-            <Text>커뮤니티</Text>
-            <ImageContainer>
-              <img src={ community } width="160" alt="" />
-            </ImageContainer>
+            <Title>커뮤니티</Title>
+            <Text>QR코드가 부착되어 있지 않은 분실물도 게시판을 통해서 찾을 수 있어요. 분실물의 유형, 분실 지역, 날자를 설정해 찾고자 하는 분실물을 쉽게 검색할 수 있어요.</Text>
           </GuideContainer>
         </ContentsContainer>
       </LandingContainer>

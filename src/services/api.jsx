fef934d49c6a02e548a737ad6ac0fce980fe70c2
Suryun Park <tnfus1999@gmail.com>
2024-04-08 18:13:13 +0900

    Fix: withCredentials header 밖으로 빼줌

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URI,
-  timeout: 5000,
-  headers: {
-    'Content-Type': 'application/json',
+    baseURL: import.meta.env.VITE_BASE_URI,
+    timeout: 5000,
+    headers: {
+        'Content-Type': 'application/json',
+    },
     withCredentials: 'true',
-  },
 });
 
 // 요청 인터셉터
